d6bc7f95963d6e05609a160a8a66ea9cc7cc2c67
Matt <molenick@users.noreply.github.com>
2025-04-09 21:58:40 -0700

    Align tests and README examples (#23)

--- src/lib.rs
+++ src/lib.rs
@@ -262,10 +262,12 @@ mod tests {
         assert_eq!(response.bytes().await.unwrap(), "potatoes".as_bytes());
         tb.shutdown().await.unwrap();
 
+        // with port
         let tb = tube!("potatoes".as_bytes(), 6301).await.unwrap();
         assert_eq!(tb.url(), "http://0.0.0.0:6301".to_string());
         tb.shutdown().await.unwrap();
 
+        // with port and status
         let tb = tube!("potatoes".as_bytes(), 6301, StatusCode::BAD_GATEWAY)
             .await
             .unwrap();
@@ -276,6 +278,7 @@ mod tests {
         assert_eq!(response.bytes().await.unwrap(), "potatoes".as_bytes());
         tb.shutdown().await.unwrap();
 
+        // with port, status and headers
         let mut headers = crate::axum::http::HeaderMap::new();
         headers.append("pasta", crate::axum::http::HeaderValue::from_static("yum"));
         let tb = tube!(

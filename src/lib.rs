45d1dee799c8e5f7c259628e37a8ffb0a3fc8620
mo <molenick@users.noreply.github.com>
2025-05-15 00:30:58 -0700

    Revert "Make delay a bit more precise"
    This reverts commit a05b94bcd2fdbe1f7afa515a12dae302beed4453.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::task::{Context, Poll};
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 use axum::http::HeaderMap;
 use axum::http::StatusCode;
@@ -177,8 +177,6 @@ impl Tube {
         crate::axum::extract::State(config): crate::axum::extract::State<TubeConfig>,
         range: Option<axum_extra::TypedHeader<axum_extra::headers::Range>>,
     ) -> impl crate::axum::response::IntoResponse {
-        let init_at = Instant::now();
-
         let len = config.body.data.len() as u64;
         let body = axum_range::KnownSize::sized(config.body, len);
 
@@ -211,8 +209,7 @@ impl Tube {
             }
         };
 
-        let remaining_delay = init_at.elapsed() - config.delay;
-        tokio::time::sleep(remaining_delay).await;
+        tokio::time::sleep(config.delay).await;
 
         response
     }
